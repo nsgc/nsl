3f07625a75e4a5e03afb1c37faf0f2e591b48fb2
nsgc <naoki@nsgc.asia>
2011-10-20 18:26:23 +0900

    結果テーブルへのデータ格納を修正

--- src/asia/nsgc/nsl/Activity/ResultActivity.java
+++ src/asia/nsgc/nsl/Activity/ResultActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -23,22 +24,18 @@ public class ResultActivity extends BaseActivity {
 		setContentView(R.layout.result);
 
 		Intent intent = getIntent();
-		Integer dailyLastPrice = selectDailyLastPrice(intent.getIntExtra("Price", 0));
-		
-		insertDailyLastPrice(dailyLastPrice);
+		int price = intent.getIntExtra("Price", 0);
 
+		Integer dailyLastPrice = selectDailyLastPrice(price);
+		insertDailyLastPrice(dailyLastPrice);
 		TextView dr = (TextView) findViewById(R.id.daily_result_id);
 		dr.setText(dailyLastPrice.toString());
 
-		Integer monthlyLastPrice = insertAndSelectMonthlyLastPrice(dailyLastPrice);
-
 		TextView mr = (TextView) findViewById(R.id.monthly_result_id);
-		mr.setText(monthlyLastPrice.toString());
-
-		Integer totalLastPrice = insertAndSelectTotalLastPrice(monthlyLastPrice);
+		mr.setText(upsertTableToTotalPrice(price, "MonthlyResult").toString());
 
 		TextView tr = (TextView) findViewById(R.id.total_result_id);
-		tr.setText(totalLastPrice.toString());
+		tr.setText(upsertTableToTotalPrice(price, "TotalResult").toString());
 
 		Button button = (Button) findViewById(R.id.to_main_button_id);
 		button.setOnClickListener(new View.OnClickListener() {
@@ -51,80 +48,56 @@ public class ResultActivity extends BaseActivity {
 		});
 	}
 
-	private Integer insertAndSelectTotalLastPrice(Integer monthlyLastPrice) {
+	/**
+	 * 同月(同日)がある場合はpriceを足して更新、初めての月(初めての日)の場合はpriceを足して登録、初めての利用の場合はpriceで登録.
+	 * 
+	 * @param price
+	 *            我慢・喫煙による金額の変動を渡す
+	 * @param targetTable
+	 *            月毎の結果テーブル(MonthlyResult) or 総計の結果テーブル(TotalResult)
+	 * @return lastPrice これまでの金額総計を返す
+	 */
+	private Integer upsertTableToTotalPrice(Integer price, String targetTable) {
+		Integer lastPrice = 7;
+		int substringEnd = 0;
+		String selectDateFormat = "yyyy/MM/dd";
+		if (targetTable.equals("TotalResult")) {
+			substringEnd = 4;
+			selectDateFormat = "yyyy/MM";
+		}
 		MyDBHelper helper = new MyDBHelper(this.getApplicationContext());
 		SQLiteDatabase db = helper.getWritableDatabase();
 
+		SimpleDateFormat fmt = new SimpleDateFormat(selectDateFormat);
+		String today = fmt.format(new GregorianCalendar().getTime());
+
+		Cursor cursor = db.query(targetTable, new String[] { "total_price", "date" }, "date like ?", new String[] { today.substring(0, substringEnd) + "%" }, null, null, "date DESC", "1");
 		ContentValues values = new ContentValues();
-		GregorianCalendar cal = new GregorianCalendar();
-		SimpleDateFormat fmt = new SimpleDateFormat("yyyy/MM");
-		String today = fmt.format(cal.getTime());
-
-		Cursor mc = db.query("TotalResult", new String[] { "total_price",
-				"date" }, "date = ?", new String[] { today }, null,
-				null, "date DESC", "1");
-		mc.moveToFirst();
-
-		Integer totalLastPrice = monthlyLastPrice;
-		
-		if (mc.moveToFirst()) {
-			//totalLastPrice = mc.getInt(0) + monthlyLastPrice;
-			totalLastPrice = monthlyLastPrice;
-			values.put("total_price", totalLastPrice);
-			db.update("TotalResult", values, "date = ?",
-					new String[] { fmt.format(cal.getTime()) });
+		if (cursor.moveToFirst()) {
+			if (cursor.getString(cursor.getColumnIndex("date")).equals(today)) {
+				lastPrice = cursor.getInt(cursor.getColumnIndex("total_price"))
+						+ price;
+				values.put("total_price", lastPrice);
+				db.update(targetTable, values, "date = ?",
+						new String[] { today });
+			} else {
+				lastPrice = cursor.getInt(cursor.getColumnIndex("total_price"))
+						+ price;
+				values.put("total_price", lastPrice);
+				values.put("date", today);
+				db.insert(targetTable, null, values);
+			}
 		} else {
-			totalLastPrice = monthlyLastPrice;
-			values.put("total_price", totalLastPrice);
-			// 新しい月の場合
+			lastPrice = price;
+			values.put("total_price", lastPrice);
 			values.put("date", today);
-			db.insert("TotalResult", null, values);
+			db.insert(targetTable, null, values);
 		}
-
-		mc.close();
+		cursor.close();
 		db.close();
 		helper.close();
-		return totalLastPrice;
-	}
 
-	private Integer insertAndSelectMonthlyLastPrice(Integer dailyLastPrice) {
-        MyDBHelper helper = new MyDBHelper(this.getApplicationContext());
-        SQLiteDatabase db = helper.getWritableDatabase();
-
-        ContentValues values = new ContentValues();
-        GregorianCalendar cal = new GregorianCalendar();
-        SimpleDateFormat fmt = new SimpleDateFormat("yyyy/MM/dd");
-
-        String today = fmt.format(cal.getTime());
-
-        Cursor mc = db.query("MonthlyResult", new String[] { "total_price", "date" }, "date like ?", new String[] {today.substring(0, 7) + "%"}, null, null, "date DESC", "1");
-
-        Integer monthlyLastPrice;
-        if (mc.moveToFirst()) {
-        	if (mc.getString(mc.getColumnIndex("date")).equals(today)) {
-        		monthlyLastPrice = dailyLastPrice;
-        		values.put("total_price", monthlyLastPrice);
-        		// 同日の場合
-        		db.update("MonthlyResult", values, "date = ?",  new String[] {today});
-        	} else {
-           		monthlyLastPrice = mc.getInt(mc.getColumnIndex("total_price")) + dailyLastPrice;
-        		values.put("total_price", monthlyLastPrice);
-        		// 新しい日で初めて起動した場合
-           		values.put("date", today);
-        		db.insert("MonthlyResult", null, values);
-        	}
-        } else {
-          	// 条件：月が変わっている場合は...
-            monthlyLastPrice = dailyLastPrice;
-      		values.put("total_price", monthlyLastPrice);
-       		values.put("date", today);
-       		db.insert("MonthlyResult", null, values);
-        }
-
-        mc.close();
-        db.close();        
-        helper.close();
-		return monthlyLastPrice;
+		return lastPrice;
 	}
 
 	private Integer selectDailyLastPrice(Integer price) {
@@ -134,13 +107,15 @@ public class ResultActivity extends BaseActivity {
 		GregorianCalendar cal = new GregorianCalendar();
 		SimpleDateFormat fmt = new SimpleDateFormat("yyyy/MM/dd");
 
-		Cursor c = db.query("DailyResult", new String[] { "date", "total_price" },
-				"date like ?",
-				new String[] { fmt.format(cal.getTime()) + "%" }, null, null, "date DESC", "1");
+		Cursor c = db.query("DailyResult",
+				new String[] { "date", "total_price" }, "date like ?",
+				new String[] { fmt.format(cal.getTime()) + "%" }, null, null,
+				"date DESC", "1");
 		Integer lastPriceForTable;
 
 		if (c.moveToFirst()) {
-			lastPriceForTable = c.getInt(c.getColumnIndex("total_price")) + price;
+			lastPriceForTable = c.getInt(c.getColumnIndex("total_price"))
+					+ price;
 		} else {
 			// 新しい日で初めて起動した場合、priceの値をセットする
 			lastPriceForTable = price;
@@ -151,16 +126,15 @@ public class ResultActivity extends BaseActivity {
 		return lastPriceForTable;
 	}
 
-	private void insertDailyLastPrice(Integer dailyLastPrice) {
+	private void insertDailyLastPrice(Integer price) {
 		MyDBHelper helper = new MyDBHelper(this.getApplicationContext());
 		SQLiteDatabase db = helper.getWritableDatabase();
 
 		ContentValues values = new ContentValues();
-		GregorianCalendar cal = new GregorianCalendar();
 		SimpleDateFormat fmt = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss Z");
 
-		values.put("date", fmt.format(cal.getTime()));
-		values.put("total_price", (Integer) dailyLastPrice);
+		values.put("date", fmt.format(new GregorianCalendar().getTime()));
+		values.put("total_price", (Integer) price);
 		db.insert("DailyResult", "", values);
 
 		db.close();
